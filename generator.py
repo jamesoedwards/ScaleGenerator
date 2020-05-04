2a3cebd5c2b18fecfcb14046327e1a91c19b76eb
jamesoedwards <james.o.edwards@gmail.com>
2020-05-04 15:35:05 +0100

    Made generator loop

--- generator.py
+++ generator.py
@@ -2,10 +2,26 @@ import random
 from keylist import KEYLIST
 from scalelist import SCALELIST
 
-def main():
+def generate():
     key   = random.choice(KEYLIST)
     scale = random.choice(SCALELIST)
 
-    print(key, scale)
+    ret = key + " " + scale
+    return ret
+
+def main():
+    print("\n>>> ScaleGenerator by James Edwards\n")
+    print("--- press q to quit ---\n\n")
+
+    cont = True
+    while cont:
+        scale = generate()
+        print(">>>", scale)
+        uInpt = input()
+        if uInpt == "q" or uInpt == "Q":
+            cont = False
+    # End: while
+    
+    return
 
 main()
