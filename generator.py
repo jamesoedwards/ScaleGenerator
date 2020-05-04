28641b7e608c1a55619e6d088cd8ac15822e3a3c
jamesoedwards <james.o.edwards@gmail.com>
2020-05-04 16:11:19 +0100

    Added scale guide and help functionality

--- generator.py
+++ generator.py
@@ -1,23 +1,32 @@
 import random
 from keylist import KEYLIST
 from scalelist import SCALELIST
+from scaleguide import SCALEMAP
 
 def generate():
     key   = random.choice(KEYLIST)
     scale = random.choice(SCALELIST)
 
-    ret = key + " " + scale
-    return ret
+    return key, scale
+
+def getScaleGuide(scale):
+    return SCALEMAP[scale]
 
 def main():
     print("\n>>> ScaleGenerator by James Edwards\n")
+    print("--- press h for help --")
     print("--- press q to quit ---\n\n")
 
     cont = True
     while cont:
-        scale = generate()
-        print(">>>", scale)
+        key, scale = generate()
+        print(">>>", key, scale)
         uInpt = input()
+        if uInpt == "h" or uInpt == "H":
+            guide = getScaleGuide(scale)
+            print(guide)
+            uInpt = input()
+
         if uInpt == "q" or uInpt == "Q":
             cont = False
     # End: while
